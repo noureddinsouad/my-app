abdefe6c6fdc77fad6f0d170a2c7cfa52e9a5c40
noureddinsouad <45936333+noureddinsouad@users.noreply.github.com>
2020-12-28 22:10:38 +0100

    fitrbase autha
    firebase autha

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './service/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
-}
+  
+  constructor(
+    public authService:AuthService
+    ){
+
+    }
+     
+    email: string;
+    password: string;
+     
+    signUp() {
+    this.authService.SignUp(this.email, this.password);
+    this.email = '';
+    this.password = '';
+    }
+     
+    signIn() {
+    this.authService.SignIn(this.email, this.password);
+    this.email = '';
+    this.password = '';
+    }
+     
+    signOut() {
+    this.authService.SignOut();
+    }
+     
+    }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from '@angular/fire';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { environment } from '../environments/environment'
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireMessagingModule } from '@angular/fire/messaging';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { FormsModule } from '@angular/forms';
+import {AuthService} from '../app/service/auth.service';
+import { AngularFirestore } from '@angular/fire/firestore';
 @NgModule({
   declarations: [
     AppComponent
   ],
+
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFireMessagingModule,
+    AngularFireStorageModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    AngularFirestore,
+    AuthService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
